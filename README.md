55163228d0dce8d19e6bebdaaa4707656eabcf5c
Kevin Latusinski <kevin.latusinski@gmail.com>
2018-04-01 16:41:18 +0200

    adjusted README

--- README.md
+++ README.md
@@ -1,18 +1,6 @@
-# theme-switcher README
+# Theme Switcher for VS Code
 
 ThemeSwitcher provides a shortcut to switch between the _Default Dark+_ and the _Default Light+_ theme. Just hit ctrl+alt+T to change the current theme.
-
-## Features
-
-
-## Extension Settings
-
-```TODO(KL): Configure dark/light themes.```
-
-This extension contributes the following settings:
-
-* `myExtension.enable`: enable/disable this extension
-* `myExtension.thing`: set to `blah` to do something
 
 ## Release Notes
 
--- src/extension.ts
+++ src/extension.ts
@@ -6,16 +6,16 @@ const THEME_DARK = 'Default Dark+';
 const THEME_LIGHT = 'Default Light+';
 
 export function activate(context: vscode.ExtensionContext) {
-    let disposable = vscode.commands.registerCommand('theme_swapper.swapTheme', () => {
-        const currentTheme = (vscode.workspace.getConfiguration().get(PROP_THEME) as string).toLowerCase();
-        if (currentTheme.includes('light')) {
-            vscode.workspace.getConfiguration().update(PROP_THEME, THEME_DARK, vscode.ConfigurationTarget.Global);
-        } else {
-            vscode.workspace.getConfiguration().update(PROP_THEME, THEME_LIGHT, vscode.ConfigurationTarget.Global);
-        }
-    });
+  let disposable = vscode.commands.registerCommand('theme_swapper.swapTheme', () => {
+    const currentTheme = (vscode.workspace.getConfiguration().get(PROP_THEME) as string).toLowerCase();
+    if (currentTheme.includes('light')) {
+      vscode.workspace.getConfiguration().update(PROP_THEME, THEME_DARK, vscode.ConfigurationTarget.Global);
+    } else {
+      vscode.workspace.getConfiguration().update(PROP_THEME, THEME_LIGHT, vscode.ConfigurationTarget.Global);
+    }
+  });
 
-    context.subscriptions.push(disposable);
+  context.subscriptions.push(disposable);
 }
 
 // this method is called when your extension is deactivated
