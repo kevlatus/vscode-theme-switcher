316e2d356ae4071b3cfaff3ee14e15ddafc2ecab
Kevin Latusinski <me@kevlatus.de>
2020-03-15 10:31:23 +0100

    refactor: reorganize settings code and add comments

--- src/extension.ts
+++ src/extension.ts
@@ -1,46 +1,88 @@
 'use strict';
 import * as vscode from 'vscode';
 
-export const NAMESPACE = 'theme_switcher';
-export const COMMAND_SWITCH = `${NAMESPACE}.switchTheme`;
-export const PROP_THEME = 'workbench.colorTheme';
-export const PROP_DARK_THEME = `${NAMESPACE}.dark_theme`;
-export const PROP_LIGHT_THEME = `${NAMESPACE}.light_theme`;
+/**
+ * The ID of this extension as configured in the package.json.
+ */
+export const EXT_ID = 'theme_switcher';
+/**
+ * The ID of the switch command as configured in the package.json.
+ */
+export const COMMAND_SWITCH = `${EXT_ID}.switchTheme`;
 
-function getDarkTheme(): string {
-  return vscode.workspace.getConfiguration().get(PROP_DARK_THEME);
+module Settings {
+  /**
+   * The name of the vscode theme settings property.
+   */
+  const SETTING_THEME = 'workbench.colorTheme';
+  /**
+   * The name of this extension's dark theme setting.
+   */
+  const SETTING_DARK_THEME = `${EXT_ID}.dark_theme`;
+  /**
+   * The name of this extension's light theme setting.
+   */
+  const SETTING_LIGHT_THEME = `${EXT_ID}.light_theme`;
+
+  /**
+   * Retrieves and returns the configured dark theme from vscode settings.
+   */
+  export function dark(): string {
+    return vscode.workspace.getConfiguration().get(SETTING_DARK_THEME);
+  }
+
+  /**
+   * Retrieves and returns the configured light theme from vscode settings.
+   */
+  export function light(): string {
+    return vscode.workspace.getConfiguration().get(SETTING_LIGHT_THEME);
+  }
+
+  /**
+   * Retrieves and returns the currently active theme from vscode settings.
+   */
+  export function current(): string {
+    return vscode.workspace.getConfiguration().get(SETTING_THEME);
+  }
+
+  /**
+   * Retrieves and returns the theme, which should be activated next.
+   * If the current theme is light, returns the dark theme and vice-versa.
+   */
+  export function next(): string {
+    const currentTheme = current();
+    const lightTheme = light();
+    const darkTheme = dark();
+
+    return currentTheme === lightTheme ? darkTheme : lightTheme;
+  }
+
+  /**
+   * Sets the global color theme setting with the given value.
+   *
+   * @param theme The theme which should be activated.
+   */
+  export function set(theme: string): void {
+    vscode.workspace
+      .getConfiguration()
+      .update(SETTING_THEME, theme, vscode.ConfigurationTarget.Global);
+  }
 }
 
-function getLightTheme(): string {
-  return vscode.workspace.getConfiguration().get(PROP_LIGHT_THEME);
-}
-
-function getCurrentTheme(): string {
-  return vscode.workspace.getConfiguration().get(PROP_THEME);
-}
-
-function getNextTheme(): string {
-  const currentTheme = getCurrentTheme();
-  const lightTheme = getLightTheme();
-  const darkTheme = getDarkTheme();
-
-  return currentTheme === lightTheme ? darkTheme : lightTheme;
-}
-
-function setTheme(theme: string): void {
-  vscode.workspace.getConfiguration().update(PROP_THEME, theme, vscode.ConfigurationTarget.Global);
-}
-
+/**
+ * Switches the vscode theme to the opposite of the currently active theme.
+ */
 async function switchTheme() {
-  const nextTheme = getNextTheme();
-  setTheme(nextTheme);
+  Settings.set(Settings.next());
 }
 
 export function activate(context: vscode.ExtensionContext) {
-  const disposable = vscode.commands.registerCommand(COMMAND_SWITCH, switchTheme);
+  const disposable = vscode.commands.registerCommand(
+    COMMAND_SWITCH,
+    switchTheme,
+  );
   context.subscriptions.push(disposable);
 }
 
 // this method is called when your extension is deactivated
-export function deactivate() {
-}
+export function deactivate() {}
